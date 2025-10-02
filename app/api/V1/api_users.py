6a426923d42114922a9ef699d35e69f3ecd18dd0
Timi-Holberton <timi.holberton@gmail.com>
2025-10-02 15:39:10 +0200

    Feat : api user post get
    Fonction POST GET GET all fonctionnel
    Fonction PUT non tester (manque token)

--- app/api/V1/api_users.py
+++ app/api/V1/api_users.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request
 from app.services import facade
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, get_jwt
 from app.models import user
@@ -12,53 +12,45 @@ class UserList(Resource):
     def post(self):
         data = request.json
         if not data:
-            return ({"error": "Données manquantes"}), 400
+            return {"error": "Données manquantes"}, 400
 
         try:
             new_user = facade.create_user(data)
-            return ({
+            return {
             "id": new_user.id,
             "first_name": new_user.first_name,
             "last_name": new_user.last_name,
             "email": new_user.email,
             "is_admin": new_user.is_admin
-            }), 201
+            }, 201
 
         except ValueError as error:
             return ({"error": str(error)}), 400
 
 # ---------- ROUTE GET /users ---------- Récupérer tous les utilisateurs
     def get(self):
-        users = facade.get_all_users()
+        users = facade.get_all_user()
         if not users:
-            return ({"error": "No users found"}), 404
+            return {"error": "No users found"}, 404
 
-        users_list = [{
-            "id": user.id,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "email": user.email,
-            "is_admin": user.is_admin
-        } for user in users]
-
-        return (users_list), 200
+        return users, 200
 
 
 # ---------- ROUTE GET /users ---------- Récupérer un utilisateur par ID
 @api.route('/<int:user_id>')
 class UserResource(Resource):
-    def get_user(self, user_id):
+    def get(self, user_id):
         user = facade.get_users(user_id)
         if not user:
-            return jsonify({"error": "User not found"}), 404
+            return {"error": "User not found"}, 404
 
-        return jsonify({
+        return {
             "id": user.id,
             "first_name": user.first_name,
             "last_name": user.last_name,
             "email": user.email,
             "is_admin": user.is_admin
-        }), 200
+        }, 200
 
 # ---------- ROUTE PUT /users/<id> ---------- Mettre à jour un utilisateur (seul l’utilisateur peut mettre à jour ses données)
     @jwt_required()
@@ -75,10 +67,10 @@ class UserResource(Resource):
         except ValueError as e:
             return jsonify({"error": str(e)}), 400
 
-        return jsonify({
+        return {
             "id": user.id,
             "first_name": user.first_name,
             "last_name": user.last_name,
             "email": user.email,
             "is_admin": user.is_admin
-        }), 200
+        }, 200

aa356e4a51254e93453e6af8a29744a5f8d62b59
Robin-1998 <10616@holbertonstudents.com>
2025-10-03 18:31:58 +0200

    Fix: Review_repository correctif
    - Ajout d'un get user by id pour récupéré les commentaires d'un utilisateur directement en repository spécifique à review

--- app/persistence/review_repository.py
+++ app/persistence/review_repository.py
@@ -1,7 +1,17 @@
-from app.models.review import Review
 from app.persistence.repository import SQLAlchemyRepository
+from app.models.review import Review
 
 class ReviewRepository(SQLAlchemyRepository):
     """Repository spécifique pour les commentaires."""
+    
+    def __init__(self):
+        """Initialise le repository avec le modèle Review"""
+        super().__init__(Review)
+    
     def get_by_post_image_id(self, image_post_id):
-        return Review.query.filter_by(image_post_id=image_post_id).all()
+        """Récupère tous les commentaires d'une image spécifique"""
+        return self.model.query.filter_by(image_post_id=image_post_id).all()
+    
+    def get_by_user_id(self, user_id):
+        """Récupère tous les commentaires d'un utilisateur"""
+        return self.model.query.filter_by(user_id=user_id).all()

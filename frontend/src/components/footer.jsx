ee28aab7857929be7ae5c675cd57c1780eb6aed8
Robin-1998 <10616@holbertonstudents.com>
2025-10-14 18:06:32 +0200

    Feat: Ajout d'images + composants footer et nav
    - Ajout des images pour le menu de navigation
    - Ajout d'une première police d'écriture
    - Composants de fonction pour footer et navigation
    - Ajouts des pages footer_mentions pour les mentions légales et footer_sources pour les sources
    - début de style des pages principales dans index.css

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,1 +1,16 @@
-function Footer
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <footer>
+		<nav>
+			<ul>
+				<li><Link to="/mentions-legales">Mentions légales</Link></li>
+				<li><Link to="/sources">Sources</Link></li>
+			</ul>
+		</nav>
+    </footer>
+  );
+};
+
+export default Footer;

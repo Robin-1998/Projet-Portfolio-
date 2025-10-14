1e1ce5a7ace41875786f5be0c07f4677faa28f4c
Timi-Holberton <timi.holberton@gmail.com>
2025-10-14 18:49:24 +0200

    Feat: Ajout de BrowserRouter et favicon dans main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from './app.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import './styles/layout.css'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
+
+/* ajout favicon */
+const link = document.createElement('link');
+link.rel = 'icon';
+link.href = favicon;
+document.head.appendChild(link);
+
+/*
+React.StrictMode : active des alertes pour t’aider à écrire un meilleur code React (en mode dev seulement).
+BrowserRouter : gère la navigation sans rechargement, indispensable pour React Router.
+*/

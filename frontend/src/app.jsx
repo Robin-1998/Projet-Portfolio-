9e936ab6e5e7058b32884aed991d9e4e31fb576e
Timi-Holberton <timi.holberton@gmail.com>
2025-10-15 16:16:27 +0200

    Refactor: Réorganisation de la structure de l'application avec ajout de BrowserRouter et Routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,13 +1,26 @@
 import Header from './components/header';
-import Navigation from './components/navigation';
+import { Routes, Route } from 'react-router-dom';
+import BodyAllPage from './components/body_all_page';
 import Footer from './components/footer';
+import routes from './routes/routes';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <>
-      <Header />
-      <Navigation />
+    <BrowserRouter>
+      <Header>
+        <Routes>
+            {/* {routes.map(({ path, element }, index) => (
+              <Route key={index} path={path} element={element} />
+            ))} */}
+        </Routes>
+      </Header>
+      <BodyAllPage />
       <Footer />
+    </BrowserRouter>
     </>
   );
 }

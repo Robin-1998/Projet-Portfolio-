62dd4e3bb34100906d25fbbabd4e0319803b9f04
Timi-Holberton <timi.holberton@gmail.com>
2025-10-15 16:16:34 +0200

    Refactor: Suppression de BrowserRouter autour de l'application dans main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
-import { BrowserRouter } from 'react-router-dom';
 import './styles/layout.css'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 )
 

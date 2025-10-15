4bfe60f4956d8afd5a2bf367ad06c31e3b66dd6c
Timi-Holberton <timi.holberton@gmail.com>
2025-10-15 17:34:44 +0200

    Refactor: Réorganisation de la structure du composant Navigation pour inclure le menu hamburger

--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -7,13 +7,21 @@ import racesImage from '../assets/logo_races.png';
 
 function Navigation() {
   return (
-	<ul className='icone_barre'>
-		<li><Link to="/onglet-carte-interactive"><img src={carteImage} alt='logo carte interactive' className='image_nav'></img></Link></li>
-		<li><Link to="/onglet-Personnages"><img src={characterImage} alt='logo personnages' className='image_nav'></img></Link></li>
-		<li><Link to="/onglet-Personnages"><img src={racesImage} alt='logo races' className='image_nav'></img></Link></li>
-		<li><Link to="/onglet-Personnages"><img src={histoireImage} alt='logo histoires' className='image_nav'></img></Link></li>
-		<li><Link to="/onglet-Personnages"><img src={artImage} alt='logo création artistique' className='image_nav'></img></Link></li>
-	</ul>
+	<nav className="menu-gauche">
+
+		<ul className='icone_barre'>
+			<div className="hamburger-menu">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<li><Link to="/onglet-carte-interactive"><img src={carteImage} alt='logo carte interactive' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={characterImage} alt='logo personnages' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={racesImage} alt='logo races' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={histoireImage} alt='logo histoires' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={artImage} alt='logo création artistique' className='image_nav'></img></Link></li>
+		</ul>
+	</nav>
   );
 };
 

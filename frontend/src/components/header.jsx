ecdd514d6c838a0cdaab30c774c47b2efb342d14
Timi-Holberton <timi.holberton@gmail.com>
2025-10-15 16:18:19 +0200

    Refactor: Remplacement des boutons de navigation par des liens dans le composant Header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,16 +1,19 @@
-import '../styles/layout.css'; // extension .css obligatoire
 import logoArbre from '../assets/logo_arbre.png';
+import { Link } from 'react-router-dom';
+import '../styles/layout.css'
 
 function Header() {
     return (
         <header className="header">
             <div className="left-side">
-                <img
-                    src={logoArbre}
-                    alt="Logo du site, représentant l'arbre blanc du Gondor"
-                    className="logo"
-                />
-                <h1>Le Seigneur des Anneaux</h1>
+                <Link to="/"> {/* lien vers la page d'accueil */}
+                    <img
+                        src={logoArbre}
+                        alt="Logo du site, représentant l'arbre blanc du Gondor"
+                        className="logo"
+                    />
+                </Link>
+                <h1 className='titre-site'>Le Seigneur des Anneaux</h1>
             </div>
 
             <div className="right-side">
@@ -18,7 +21,9 @@ function Header() {
                     <input type="text" placeholder="Rechercher un lieu, un personnage..." />
                     <button>🔍</button>
                 </div>
-                <button className="login-bouton">Connexion</button>
+                <Link to="/login">
+                    <button className="login-bouton">Connexion</button>
+                </Link>
             </div>
         </header>
     );

790f4a91fd75928c144e1d63def51210ed91f993
Timi-Holberton <timi.holberton@gmail.com>
2025-10-16 18:03:41 +0200

    feat: Ajouter le composant Navigation avec le menu de navigation

---
+++ frontend/src/components/static_components/Navigation.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import carteImage from '../../assets/logo_carte.png';
+import characterImage from '../../assets/logo_personnage.png';
+import histoireImage from '../../assets/logo_livre.png';
+import artImage from '../../assets/logo_creation.png';
+import racesImage from '../../assets/logo_races.png';
+
+function Navigation() {
+  return (
+	<nav className="menu-gauche">
+		<ul className='icone_barre'>
+			<div className="hamburger-menu">
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<li><Link to="/onglet-carte-interactive"><img src={carteImage} alt='logo carte interactive' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={characterImage} alt='logo personnages' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={racesImage} alt='logo races' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={histoireImage} alt='logo histoires' className='image_nav'></img></Link></li>
+			<li><Link to="/onglet-Personnages"><img src={artImage} alt='logo création artistique' className='image_nav'></img></Link></li>
+		</ul>
+	</nav>
+  );
+};
+
+export default Navigation;

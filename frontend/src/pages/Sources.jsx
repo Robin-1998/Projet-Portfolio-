0dfe5a4b14d3d77c43f9a3c7ff6732e7635fc918
Robin-1998 <10616@holbertonstudents.com>
2025-10-16 17:44:19 +0200

    feat: Ajout des composants Mentions et Sources

---
+++ frontend/src/pages/Sources.jsx
@@ -0,0 +1,14 @@
+import BodyPage from '../components/static_components/Body_page';
+import MentionsSource from '../components/dynamic_components/Footer_sources';
+
+function Sources() {
+	return (
+		<>
+			<BodyPage>
+				<MentionsSource />
+			</BodyPage>
+		</>
+	);
+}
+
+export default Sources

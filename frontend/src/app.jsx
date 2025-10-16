e76a94e7be4c00c118419c1145749caf07d80f1d
Timi-Holberton <timi.holberton@gmail.com>
2025-10-16 11:45:38 +0200

    refactor: Suppression des composants Header, BodyAllPage et Footer dans App

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,25 +1,17 @@
-import Header from './components/static_components/header';
 import { Routes, Route } from 'react-router-dom';
-import BodyAllPage from './components/static_components/body_all_page';
-import Footer from './components/static_components/footer';
 import routes from './routes/routes';
 import { BrowserRouter } from 'react-router-dom';
-import Login from './pages/Login';
 
 
 function App() {
   return (
     <>
     <BrowserRouter>
-      <Header>
         <Routes>
-            {/* {routes.map(({ path, element }, index) => (
+            {routes.map(({ path, element }, index) => (
               <Route key={index} path={path} element={element} />
-            ))} */}
+            ))}
         </Routes>
-      </Header>
-      <BodyAllPage />
-      <Footer />
     </BrowserRouter>
     </>
   );

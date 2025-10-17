e2e983dc8c8e924a2008866b57882edb841ff73d
Timi-Holberton <timi.holberton@gmail.com>
2025-10-17 17:30:45 +0200

    fix: Ajouter CORS et configurer l'hôte et le port pour l'application Flask

--- run.py
+++ run.py
@@ -3,10 +3,11 @@ Ce script est le point d’entrée de l’application Flask.
 Il crée l’application via create_app() et la lance en mode debug si le
 fichier est exécuté directement, suivant une architecture Flask modulaire.
 """
-
 from backend.app import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)

f9a8a70ce0ed6a56b79379d7abd2156f3ee94d9a
Timi-Holberton <timi.holberton@gmail.com>
2025-10-17 17:56:34 +0200

    feat: Réorganiser le composant Navigation avec gestion de l'état du menu et ajout de texte descriptif

--- frontend/src/components/static_components/Navigation.jsx
+++ frontend/src/components/static_components/Navigation.jsx
@@ -5,23 +5,51 @@ import histoireImage from '../../assets/logo_livre.png';
 import artImage from '../../assets/logo_creation.png';
 import racesImage from '../../assets/logo_races.png';
 
-function Navigation() {
+function Navigation({ menuOpen, setMenuOpen }) {
   return (
-	<nav className="menu-gauche">
-		<ul className='icone_barre'>
-			<div className="hamburger-menu">
-				<span></span>
-				<span></span>
-				<span></span>
-			</div>
-			<li><Link to="/onglet-carte-interactive"><img src={carteImage} alt='logo carte interactive' className='image_nav'></img></Link></li>
-			<li><Link to="/onglet-Personnages"><img src={characterImage} alt='logo personnages' className='image_nav'></img></Link></li>
-			<li><Link to="/onglet-Personnages"><img src={racesImage} alt='logo races' className='image_nav'></img></Link></li>
-			<li><Link to="/onglet-Personnages"><img src={histoireImage} alt='logo histoires' className='image_nav'></img></Link></li>
-			<li><Link to="/onglet-Personnages"><img src={artImage} alt='logo création artistique' className='image_nav'></img></Link></li>
-		</ul>
-	</nav>
+    <nav className={`menu-gauche ${menuOpen ? 'open' : ''}`}>
+      {/* Bouton hamburger */}
+      <div className="hamburger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      {/* Icônes + texte */}
+      <ul className={`icone_barre ${menuOpen ? 'visible' : ''}`}>
+        <li>
+          <Link to="/home_map" className="nav-item">
+            <img src={carteImage} alt="carte interactive" className="image_nav" />
+            {menuOpen && <span className="nav-text">Carte</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to="/personnages" className="nav-item">
+            <img src={characterImage} alt="personnages" className="image_nav" />
+            {menuOpen && <span className="nav-text">Personnages</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to="/races" className="nav-item">
+            <img src={racesImage} alt="races" className="image_nav" />
+            {menuOpen && <span className="nav-text">Races</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to="/histoires" className="nav-item">
+            <img src={histoireImage} alt="histoires" className="image_nav" />
+            {menuOpen && <span className="nav-text">Histoires</span>}
+          </Link>
+        </li>
+        <li>
+          <Link to="/art" className="nav-item">
+            <img src={artImage} alt="création artistique" className="image_nav" />
+            {menuOpen && <span className="nav-text">Gallerie d'Art</span>}
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
-};
+}
 
 export default Navigation;

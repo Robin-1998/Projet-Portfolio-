2cff025a259da6f7534918151dc55f749f746c1f
Timi-Holberton <timi.holberton@gmail.com>
2025-10-17 17:56:27 +0200

    feat: Ajouter le composant BodyPage avec navigation et structure de page

---
+++ frontend/src/components/static_components/Body_page.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Navigation from './Navigation';
+import Header from './Header';
+import Footer from './Footer';
+
+function BodyPage({ children }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className={`layout ${menuOpen ? 'open' : ''}`}>
+      <Navigation menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="content-area">
+        <Header />
+        <main className="contenu-principal">
+          {children ? children : <p>Voici le contenu principal</p>}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default BodyPage;

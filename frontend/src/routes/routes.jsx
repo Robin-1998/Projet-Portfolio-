03de4c0ef71959210980ca74486ab9242e16db98
Robin-1998 <10616@holbertonstudents.com>
2025-10-19 10:04:24 +0200

    feat: Ajouter la gestion des routes et la structure de la page principale

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Home_map from "../pages/Home_map";
 
 const routes = [
 	{ path: "/", element : <Home />},
-	{ path: "/login", element: <Login /> }
+	{ path: "/login", element: <Login /> },
+	{ path: "/Home_map", element: <Home_map /> }
 ]
 
 export default routes

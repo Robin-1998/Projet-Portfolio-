119934d070546fb41812a1822e5d5f2049e7ba22
Robin-1998 <10616@holbertonstudents.com>
2025-10-20 19:03:54 +0200

    feat: Ajouter les composants HistoryList et RacesList dans leurs pages respectives

---
+++ frontend/src/pages/Characters.jsx
@@ -0,0 +1,14 @@
+import BodyPage from '../components/static_components/Body_page';
+import CharacterList from '../components/dynamic_components/Character_list';
+
+function HistoryList() {
+	return (
+		<>
+			<BodyPage>
+				<CharacterList />
+			</BodyPage>
+		</>
+	);
+}
+
+export default HistoryList

f550ac941dc77f3c441ee63a30b5cdfb89a630a7
Timi-Holberton <timi.holberton@gmail.com>
2025-10-20 18:43:58 +0200

    style: correction de l'indentation et amélioration de la mise en forme dans le composant Formulaire_connexion

--- frontend/src/components/dynamic_components/Formulaire_connexion.jsx
+++ frontend/src/components/dynamic_components/Formulaire_connexion.jsx
@@ -6,28 +6,40 @@ function FormulaireLogin() {
     <div className="login-container">
       {/* FORMULAIRE LOGIN */}
       <form className="form-box1">
-        <h2 className='login-h2'>Connexion</h2>
-        <div className='bouton-text1'>
+        <h2 className="login-h2">Connexion</h2>
+        <div className="bouton-text1">
           <input type="email" placeholder="Email" className="input-field" />
-          <input type="password" placeholder="Mot de passe" className="input-field" />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            className="input-field"
+          />
         </div>
-        <img className='ornement' src={ornement} alt="Ornement" />
-        <button type="submit" className="btn">Se connecter</button>
+        <img className="ornement" src={ornement} alt="Ornement" />
+        <button type="submit" className="btn">
+          Se connecter
+        </button>
       </form>
 
       {/* FORMULAIRE REGISTER */}
       <form className="form-box2">
-        <h2 className='login-h2'>Créer un Hobbit</h2>
-        <div className='bouton-text2'>
+        <h2 className="login-h2">Créer un Hobbit</h2>
+        <div className="bouton-text2">
           <input type="text" placeholder="Nom" className="input-field" />
           <input type="text" placeholder="Prénom" className="input-field" />
           <input type="email" placeholder="Email" className="input-field" />
-          <input type="password" placeholder="Mot de passe" className="input-field" />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            className="input-field"
+          />
         </div>
-        <button type="submit" className="btn">Créer le compte</button>
+        <button type="submit" className="btn">
+          Créer le compte
+        </button>
       </form>
     </div>
   );
 }
 
-export default FormulaireLogin
+export default FormulaireLogin;

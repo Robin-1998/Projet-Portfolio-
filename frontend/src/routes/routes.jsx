306891157f8abbd042d33c6b6284b56fabac7cb3
Timi-Holberton <timi.holberton@gmail.com>
2025-10-20 18:36:18 +0200

    feat: Ajouter des routes pour les pages Mentions, Sources, Characters, History et Races

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,11 +1,20 @@
 import Login from "../pages/Login";
-import Home from "../pages/Home";
 import Home_map from "../pages/Home_map";
+import Mentions from "../pages/Mentions";
+import Sources from "../pages/Sources";
+import Characters from "../pages/Characters";
+import History from "../pages/History";
+import Races from "../pages/Races";
 
 const routes = [
-	{ path: "/", element : <Home />},
-	{ path: "/login", element: <Login /> },
-	{ path: "/Home_map", element: <Home_map /> }
+  { path: '/', element: <Home_map /> },
+  { path: "/login", element: <Login /> },
+  { path: "/Home_map", element: <Home_map /> },
+  { path: "/mentions", element: <Mentions />},
+  { path: "/sources", element: <Sources />},
+  { path: "/characters", element: <Characters />},
+  { path: "/histoires", element: <History />},
+  { path: "/races", element: <Races />}
 ]
 
-export default routes
+export default routes;

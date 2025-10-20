ba6323edb251d20ab9406293cfb2c12dfcbb1635
Timi-Holberton <timi.holberton@gmail.com>
2025-10-20 18:48:04 +0200

    style: ajout du paramètre mainClassName pour le composant BodyPage dans Home_map

--- frontend/src/pages/Home_map.jsx
+++ frontend/src/pages/Home_map.jsx
@@ -1,16 +1,13 @@
 import 'leaflet/dist/leaflet.css';
-import { Link } from 'react-router-dom';
 import BodyPage from '../components/static_components/Body_page';
 import Map2 from '../components/dynamic_components/Map2';
 
 function Home_map() {
   return (
-    <>
-      <BodyPage>
-        <Map2 />
-      </BodyPage>
-    </>
+    <BodyPage mainClassName="map-fullscreen">
+      <Map2 />
+    </BodyPage>
   );
 }
 
-export default Home_map
+export default Home_map;

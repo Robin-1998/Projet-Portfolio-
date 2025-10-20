05299ef2412c2e3afd4ca8dd5b16bf75470fd6e1
Timi-Holberton <timi.holberton@gmail.com>
2025-10-20 18:45:36 +0200

    style: amélioration de l'indentation et ajout de Tooltip dans le composant Header

--- frontend/src/components/static_components/Header.jsx
+++ frontend/src/components/static_components/Header.jsx
@@ -1,32 +1,37 @@
 import logoArbre from '../../assets/logo_arbre.png';
 import { Link } from 'react-router-dom';
+import Tooltip from './Tooltip';
 
 function Header() {
-    return (
-        <header className="header">
-            <div className="left-side">
-                <Link to="/"> {/* lien vers la page d'accueil */}
-                    <img
-                        src={logoArbre}
-                        alt="Logo du site, représentant l'arbre blanc du Gondor"
-                        className="logo"
-                    />
-                </Link>
-                <h1 className='titre-site'>Le Seigneur des Anneaux</h1>
-            </div>
+  return (
+    <header className="header">
+      <div className="left-side">
+        <Tooltip text="Retour à la page d'accueil">
+          <Link to="/" style={{ display: 'inline-block' }}>
+            <img
+              src={logoArbre}
+              alt="Logo du site, représentant l'arbre blanc du Gondor"
+              className="logo"
+            />
+          </Link>
+        </Tooltip>
+        <h1 className="titre-site">Le Seigneur des Anneaux</h1>
+      </div>
 
-            <div className="right-side">
-                <div className="search-bar">
-                    <input type="text" placeholder="Rechercher un lieu, un personnage..." />
-                    <button>🔍</button>
-                </div>
-                <Link to="/login">
-                    <button className="login-bouton">Connexion</button>
-                </Link>
-            </div>
-        </header>
-    );
+      <div className="right-side">
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Rechercher un lieu, un personnage..."
+          />
+          <button>🔍</button>
+        </div>
+        <Link to="/login">
+          <button className="login-bouton">Connexion</button>
+        </Link>
+      </div>
+    </header>
+  );
 }
-
 
 export default Header;

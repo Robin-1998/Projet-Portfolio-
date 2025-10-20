117aace83d424fd33de8c52712e49d3c6f649579
Timi-Holberton <timi.holberton@gmail.com>
2025-10-20 18:49:45 +0200

    style: amélioration de l'indentation et ajout d'un point-virgule manquant dans le fichier Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,16 @@
 import { Link } from 'react-router-dom';
 import BodyPage from '../components/static_components/Body_page';
 import FormulaireLogin from '../components/dynamic_components/Formulaire_connexion';
+import React, { useState } from 'react';
 
 function Login() {
-	return (
-		<>
-			<BodyPage>
-				<FormulaireLogin />
-			</BodyPage>
-		</>
-	);
+  return (
+    <>
+      <BodyPage>
+        <FormulaireLogin />
+      </BodyPage>
+    </>
+  );
 }
 
-export default Login
+export default Login;

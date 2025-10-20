f143a9e8e44c48c851a2eef5df8a7d4c9affa07a
Timi-Holberton <timi.holberton@gmail.com>
2025-10-20 18:49:38 +0200

    style: correction de l'indentation et ajout d'un point-virgule manquant dans le fichier Mentions.jsx

--- frontend/src/pages/Mentions.jsx
+++ frontend/src/pages/Mentions.jsx
@@ -2,13 +2,13 @@ import BodyPage from '../components/static_components/Body_page';
 import MentionsLegales from '../components/dynamic_components/Footer_mentions';
 
 function Mentions() {
-	return (
-		<>
-			<BodyPage>
-				<MentionsLegales />
-			</BodyPage>
-		</>
-	);
+  return (
+    <>
+      <BodyPage>
+        <MentionsLegales />
+      </BodyPage>
+    </>
+  );
 }
 
-export default Mentions
+export default Mentions;

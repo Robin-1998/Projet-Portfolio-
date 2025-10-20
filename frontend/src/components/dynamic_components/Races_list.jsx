b68444e616dabac4945ac954a21ef64cf4fe9d59
Robin-1998 <10616@holbertonstudents.com>
2025-10-20 18:37:58 +0200

    feat: Ajouter de nouveaux composants
    -CharactersListe, HistoryList et RacesList, et le filtre pour la sélection des éléments

---
+++ frontend/src/components/dynamic_components/Races_list.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+// Import de la bibliothèque axios pour faire les requêtes HTTP
+import '../../styles/liste.css';
+
+function RacesList() {
+  const [Races, setRaces] = useState([]);
+  // Déclaration de l'état local "characters" . permetttra de stocker les histoires
+  const [loading, setLoading] = useState(true);
+  // loading permet d’afficher quelque chose à l’utilisateur pendant que la page est en train de charger les données
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/api/v1/races')
+      .then(res => {
+        setRaces(res.data);
+        setLoading(false);
+      })
+	  // Cas d'erreur si jamais il y a un problème avec la récupération des évènements
+      .catch(err => {
+        console.error("Erreur lors de la récupération :", err);
+        setLoading(false);
+      });
+  }, []);
+
+  // Si c'est toujours en chargement alors on retourne le message pour en informer l'utilisateur
+  if (loading) return <p>Chargement des différentes Races...</p>;
+
+  return (
+	<div className='container-vitrine'>
+		<div className="head-filter">
+			<h1>Les Espèces</h1>
+		</div>
+		<div className="character-grid">
+			{Races.map(char => (
+				<div key={char.id} className="card">
+				<h2>{char.name}</h2>
+				<p>{char.description}</p>
+				</div>
+			))}
+		</div>
+	</ div>
+  );
+}
+
+export default RacesList;

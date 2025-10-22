f9ee3f8827b88c4a257d27dc778b9df7846e18cf
Robin-1998 <10616@holbertonstudents.com>
2025-10-22 14:47:35 +0200

    feat: Ajouter la route pour le composant RaceZoom

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -5,6 +5,7 @@ import Sources from "../pages/Sources";
 import Characters from "../pages/Characters";
 import History from "../pages/History";
 import Races from "../pages/Races";
+import RaceZoom from "../pages/Race_zoom";
 
 const routes = [
   { path: '/', element: <Home_map /> },
@@ -14,7 +15,8 @@ const routes = [
   { path: "/sources", element: <Sources />},
   { path: "/characters", element: <Characters />},
   { path: "/histoires", element: <History />},
-  { path: "/races", element: <Races />}
+  { path: "/races", element: <Races />},
+  { path: "/races/:id", element: <RaceZoom /> }
 ]
 
 export default routes;

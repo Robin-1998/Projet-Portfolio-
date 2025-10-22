1eed720baaa05a2cf5086bb206104d9ad46b7af0
Robin-1998 <10616@holbertonstudents.com>
2025-10-22 15:11:51 +0200

    Feat: Ajout fonctionalité pour info sur une race
    - Ajout du fichier css pour quelque style sur bloc + image
    - ajout du composant  race_detail pour avoir le détail de chaque race sur le clic d'une image
    - ajout du fichier race_zoom pour la page spécifique à chaque race

---
+++ frontend/src/components/dynamic_components/Races_details.jsx
@@ -0,0 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import getImagePath from '../../services/getImage';
+import '../../styles/page_information.css';
+
+function RaceDetail() {
+  const { id } = useParams();
+  const [race, setRace] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:5000/api/v1/races/${id}`)
+      .then(res => {
+        setRace(res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error("Erreur lors de la récupération :", err);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) return <p>Chargement...</p>;
+  if (!race) return <p>Race non trouvée</p>;
+
+  return (
+    <div className="bloc-info">
+      <h1>{race.name}</h1>
+      <img src={getImagePath(race.name, 'races')} alt={race.name} className="image_info"/>
+      <p>{race.description}</p>
+    </div>
+  );
+}
+
+export default RaceDetail;
---
+++ frontend/src/pages/Race_zoom.jsx
@@ -0,0 +1,14 @@
+import BodyPage from '../components/static_components/Body_page';
+import RaceDetail from '../components/dynamic_components/Races_details';
+
+function RaceZoom() {
+	return (
+		<>
+			<BodyPage>
+				<RaceDetail />
+			</BodyPage>
+		</>
+	);
+}
+
+export default RaceZoom

1082627b6df81d48b1feaa827e7dd7cab018ae95
Timi-Holberton <timi.holberton@gmail.com>
2025-10-28 09:56:13 +0100

    fix: Ajouter l'importation du fichier CSS pour le composant LoginRegister

--- frontend/src/components/dynamic_components/Login_register.jsx
+++ frontend/src/components/dynamic_components/Login_register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import FormulaireLogin from './Formulaire_connexion';
 import FormulaireRegister from './Formulaire_register';
+import '../../styles/login.css';
+
 
 function LoginRegister() {
   return (

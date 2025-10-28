15ac2f96b9c2ee385cf4880ee069ce91a7df8003
Timi-Holberton <timi.holberton@gmail.com>
2025-10-28 09:55:53 +0100

    fix: Ajouter l'importation du fichier CSS pour le formulaire de connexion

--- frontend/src/components/dynamic_components/Formulaire_connexion.jsx
+++ frontend/src/components/dynamic_components/Formulaire_connexion.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ornement from '../../assets/ornement.PNG';
 import {useNavigate} from 'react-router-dom';
+import '../../styles/login.css';
 
 
 function FormulaireLogin() {

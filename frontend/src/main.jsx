8d7c3de70d0d1b08f76079d0d6d84ea742f4d5ef
Robin-1998 <10616@holbertonstudents.com>
2025-10-28 09:41:38 +0100

    Fix: suppression des import utilisé

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.jsx';
 import './styles/static.css';
-import './styles/index.css';
-import './styles/login.css';
 import './styles/map.css';
 import './styles/Tooltip.css';
-import './styles/detail_place.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-/*
-React.StrictMode : active des alertes pour t’aider à écrire un meilleur code React (en mode dev seulement).
-BrowserRouter : gère la navigation sans rechargement, indispensable pour React Router.
-*/

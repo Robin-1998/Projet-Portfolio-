44f06f422518df6eb645bceb134a0ab6ed7fad5b
Timi-Holberton <timi.holberton@gmail.com>
2025-10-29 14:19:25 +0100

    fix: Ajouter des commentaires pour clarifier le composant Creations

--- frontend/src/pages/Creations.jsx
+++ frontend/src/pages/Creations.jsx
@@ -1,14 +1,23 @@
+/**
+ * @module Creations
+ * Page affichant la liste des créations dans un corps de page.
+ */
+
 import BodyPage from '../components/static_components/Body_page';
 import CreationsList from '../components/dynamic_components/Creations_list';
 
+/**
+ * Composant de page pour la section "Créations".
+ * @returns {JSX.Element} Conteneur affichant la liste des créations.
+ */
 function Creations() {
-	return (
-		<>
-			<BodyPage>
-				<CreationsList />
-			</BodyPage>
-		</>
-	);
+  return (
+    <>
+      <BodyPage>
+        <CreationsList />
+      </BodyPage>
+    </>
+  );
 }
 /* Ajouter ImagePost */
 export default Creations

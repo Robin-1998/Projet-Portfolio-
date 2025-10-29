c402df1c4f03784615cdb143923a35f4645e1dd7
Timi-Holberton <timi.holberton@gmail.com>
2025-10-29 14:10:53 +0100

    fix: Mettre à jour la documentation du point d'entrée pour plus de clarté

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 """
-Ce script est le point d’entrée de l’application Flask.
-Il crée l’application via create_app() et la lance en mode debug si le
-fichier est exécuté directement, suivant une architecture Flask modulaire.
+Point d’entrée de l’application Flask.
+Crée et lance l’application avec CORS activé.
 """
 from backend.app import create_app
 from flask_cors import CORS

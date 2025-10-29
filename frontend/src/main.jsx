d050975e376492e88fed68587efcb95488f3fd62
Timi-Holberton <timi.holberton@gmail.com>
2025-10-29 14:15:24 +0100

    fix: Réorganiser et clarifier les commentaires dans le point d'entrée de l'application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,15 @@
+/**
+ * Point d'entrée de l'application React
+ * @module Main
+ */
+
+/**
+ * Initialise et monte l'application React dans le DOM
+ * Configure le StrictMode pour détecter les problèmes potentiels
+ *
+ * @returns {void}
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.jsx';

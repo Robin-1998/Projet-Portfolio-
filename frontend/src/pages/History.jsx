55640d629314625676e41dcf87673de2e285b6fb
Timi-Holberton <timi.holberton@gmail.com>
2025-10-29 14:19:35 +0100

    fix: Ajouter des commentaires pour clarifier le composant History

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,14 +1,23 @@
+/**
+ * @module History
+ * Page affichant la liste des histoires dans un corps de page.
+ */
+
 import BodyPage from '../components/static_components/Body_page';
 import HistoryList from '../components/dynamic_components/History_list';
 
+/**
+ * Composant de page pour la section "Histoires".
+ * @returns {JSX.Element} Conteneur affichant la liste des histoires.
+ */
 function history() {
-	return (
-		<>
-			<BodyPage>
-				<HistoryList />
-			</BodyPage>
-		</>
-	);
+  return (
+    <>
+      <BodyPage>
+        <HistoryList />
+      </BodyPage>
+    </>
+  );
 }
 
 export default history

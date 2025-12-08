d033cbf497951108b569758e47081bef6f91b8ae
Robin-1998 <10616@holbertonstudents.com>
2025-12-08 01:07:33 +0100

    Fix: ajout de l'import Identity

--- backend/app/models/basemodel.py
+++ backend/app/models/basemodel.py
@@ -11,6 +11,7 @@ de ces fonctionnalités de base.
 """
 from backend.app import db
 from datetime import datetime, timezone
+from sqlalchemy import Identity
 
 class BaseModel(db.Model):
     """
@@ -28,7 +29,7 @@ class BaseModel(db.Model):
 
     __abstract__ = True
 
-    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    id = db.Column(db.BigInteger, Identity(always=False), primary_key=True)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc),
                           onupdate=lambda: datetime.now(timezone.utc))
@@ -64,4 +65,3 @@ class BaseModel(db.Model):
             'created_at': self.created_at.isoformat() if self.created_at else None,
             'updated_at': self.updated_at.isoformat() if self.updated_at else None
         }
-

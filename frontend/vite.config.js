1b6cbed0cd0f39487d11b36b8b3e37714fac01ec
Robin-1998 <10616@holbertonstudents.com>
2025-12-26 19:02:23 +0100

    Fix: mise à jour de la configuration du serveur pour utilisation avec contenaire docker en back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -34,10 +34,11 @@ export default defineConfig({
   },
 
   server: {
+    host: true,
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://backend:5000',
         changeOrigin: true,
       },
     },

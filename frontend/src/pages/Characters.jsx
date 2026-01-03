bb76ba3dca32dbbae6e642005784d9e3e3e12d44
Robin-1998 <10616@holbertonstudents.com>
2026-01-03 01:20:32 +0100

    Fix: Mise à jour des commentaires

--- frontend/src/pages/Characters.jsx
+++ frontend/src/pages/Characters.jsx
@@ -1,15 +1,11 @@
 /**
- * @module HistoryList
  * Page affichant la liste des personnages dans un corps de page.
  */
 
 import BodyPage from '../components/static_components/Body_page';
 import CharacterList from '../components/dynamic_components/Character_list';
 
-/**
- * Composant de page pour la section "Histoire".
- * @returns {JSX.Element} Conteneur affichant la liste des personnages.
- */
+
 function HistoryList() {
 	return (
 		<>
--- frontend/src/pages/Creations.jsx
+++ frontend/src/pages/Creations.jsx
@@ -1,15 +1,10 @@
 /**
- * @module Creations
  * Page affichant la liste des créations dans un corps de page.
  */
 
 import BodyPage from '../components/static_components/Body_page';
 import CreationsList from '../components/dynamic_components/Creations_list';
 
-/**
- * Composant de page pour la section "Créations".
- * @returns {JSX.Element} Conteneur affichant la liste des créations.
- */
 function Creations() {
   return (
     <>
